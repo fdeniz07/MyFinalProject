79ffb46e00392c697ed6ed8cdc52de8781f68401
Fatih Deniz <deniz.fatih@hotmail.de>
2023-08-23 19:30:21 +0200

    Security Utilities

---
+++ Core/Utilities/Security/Hashing/HashingHelper.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace Core.Utilities.Security.Hashing
+{
+    //Bu class'in amaci kullanicinin verdigi sifreyi Hashleyip Saltlamaktir ya da sifreyi dogrulamaktir.
+    public class HashingHelper
+    {
+        //Deger türlerini referans olarak metoda gecmek icin ref ve out keywordlari kullanilir. Her ikisi de ayni islemi yapar.
+        //ref ile kullanımda değişkene başlangıç değerinin verilmesi gerekirken
+        //out anahtar kelimesi ile kullanımda başlangıç değerinin verilmesine gerek yoktur.
+        public static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        public static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != passwordHash[i])
+                    {
+                        return false;
+                    }
+                }
+                return true; 
+            }
+        }
+    }
+}
---
+++ Core/Utilities/Security/Hashing/SigningCredentialsHelper.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.IdentityModel.Tokens;
+
+namespace Core.Utilities.Security.Hashing
+{
+    //JWT de kullanilacak key'in dogrulanmasi
+    public class SigningCredentialsHelper
+    {
+        public static SigningCredentials CreateSigningCredentials(SecurityKey securityKey)
+        {
+            return new SigningCredentials(securityKey,SecurityAlgorithms.HmacSha512Signature);
+        }
+    }
+}
